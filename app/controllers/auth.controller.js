efbe92220670fccb39c0bf098a148fbb23600db9
harryoppa <harryoppa123@gmail.com>
2022-01-09 17:56:20 +0700

    Refactor code

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -5,37 +5,38 @@ const config = require("../config/config")
 
 const User = db.user;
 
-exports.login = (req, res) => {
-  User.findOne({
-    where: {
-      email: req.body.email,
-    },
-  }).then((user) => {
+exports.login = async (req, res) => {
+  try {
+    const user = await User.findOne({
+      where: {
+        email: req.body.email,
+      },
+    });
+  
     if (!user) {
       return res.status(404).send({ message: "User Not found." });
     }
-
+  
     let passwordIsValid = bcrypt.compareSync(req.body.password, user.password);
-
+  
     if (!passwordIsValid) {
       return res.status(401).send({
         accessToken: null,
         message: "Invalid Password!",
       });
     }
-
+  
     let token = jwt.sign({ id: user.id }, config.auth.secret, {
       expiresIn: 86400, // 24 hours
     });
-
+  
     res.status(200).send({
         id: user.id,
         username: user.username,
         email: user.email,
         accessToken: token,
     });
-  })
-  .catch(err => {
+  } catch(err) {
     res.status(500).send({ message: err.message });
-  });
+  }
 };
--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -2,100 +2,102 @@ const db = require("../models");
 const bcrypt = require("bcryptjs");
 const User = db.user;
 
-exports.userInfo = (req, res) => {
-    User.findOne({
+exports.userInfo = async (req, res) => {
+    const user = await User.findOne({
         where: {
             id: req.user.id,
         },
-    }).then((user) => {
-        if (!user) {
-            return res.status(404).send({ message: "User Not found." });
-        }
-        res.status(200).send({
-            id: user.id,
-            fullname: user.fullname,
-            email: user.email,
-        });
+    });
+
+    if (!user) {
+        return res.status(404).send({ message: "User Not found." });
+    }
+    
+    res.status(200).send({
+        id: user.id,
+        fullname: user.fullname,
+        email: user.email,
     });
 }
 
-exports.userList = (req, res) => {
-    User.findAll({
+exports.userList = async (req, res) => {
+    const users = await User.findAll({
         attributes: ["id", "fullname", "email"],
         // order by created at
         order: [
             ["created_at", "ASC"],
         ],
-    }).then((users) => {
-        if (!users) {
-            return res.status(404).send({ message: "Users Not found." });
-        }
-        res.status(200).send(users);
     });
+
+    if (!users) {
+        return res.status(404).send({ message: "Users Not found." });
+    }
+    res.status(200).send(users);
 }
 
-exports.createOrUpdateUser = (req, res) => {
+exports.createOrUpdateUser = async (req, res) => {
     // check request has id
     if (req.body.id) {
         // check exist email
-        User.findOne({
+        const user = await User.findOne({
             where: {
                 email: req.body.email,
             },
+        })
+
+        if (user && user.id !== req.body.id) {
+            return res.status(400).send({ message: "Email already exists." });
+        }
+        // update user
+        User.update({
+            fullname: req.body.fullname,
+            email: req.body.email,
+            // check if password is not empty
+            password: req.body.password ? bcrypt.hashSync(req.body.password, 8) : user.password,
+        }, {
+            where: {
+                id: req.body.id,
+            },
         }).then((user) => {
-            if (user && user.id !== req.body.id) {
-                return res.status(400).send({ message: "Email already exists." });
-            }
-            // update user
-            User.update({
-                fullname: req.body.fullname,
-                email: req.body.email,
-                // check if password is not empty
-                password: req.body.password ? bcrypt.hashSync(req.body.password, 8) : user.password,
-            }, {
-                where: {
-                    id: req.body.id,
-                },
-            }).then((user) => {
-                res.status(200).send(user);
-            });
+            res.status(200).send(user);
         });
     } else {
         // check exist email
-        User.findOne({
+        const user = await User.findOne({
             where: {
                 email: req.body.email,
             },
+        });
+
+        if (user) {
+            return res.status(400).send({ message: "Email already exists." });
+        }
+        // create user
+        User.create({
+            fullname: req.body.fullname,
+            email: req.body.email,
+            password: bcrypt.hashSync(req.body.password, 8),
         }).then((user) => {
-            if (user) {
-                return res.status(400).send({ message: "Email already exists." });
-            }
-            // create user
-            User.create({
-                fullname: req.body.fullname,
-                email: req.body.email,
-                password: bcrypt.hashSync(req.body.password, 8),
-            }).then((user) => {
-                res.status(200).send(user);
-            });
+            res.status(200).send(user);
         });
     }
 }
 
-exports.deleteUser = (req, res) => {
+exports.deleteUser = async (req, res) => {
     // check if user id equal 1 deny delete
     if (req.params.id === "1") {
         return res.status(400).send({ message: "You can't delete this user." });
     }
 
-    User.destroy({
+    const user = await User.destroy({
         where: {
             id: req.params.id,
         },
-    }).then((user) => {
-        if (!user) {
-            return res.status(404).send({ message: "User Not found." });
-        }
-        res.status(200).send({ message: "User deleted successfully." });
     });
+
+    if (!user) {
+        return res.status(404).send({ message: "User Not found." });
+    }
+
+    res.status(200).send({ message: "User deleted successfully." });
 }
